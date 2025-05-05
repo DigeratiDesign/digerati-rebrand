5224fb3b459cc0716ffb34b6919caab53b43a561
Digerati <cabal@digerati.design>
2025-05-05 15:54:30 +0100

    Add 404

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import { mouseTrail } from "$digerati/mouseTrail";
 import { tallyModal } from "$digerati/tallyModal";
 import { testimonialAvatar } from "$digerati/testimonialAvatar";
 import { convertMarkdownToTable } from "$digerati/convertMarkdownToTable";
+import { reasonGenerator } from "$digerati/reasonGenerator";
 
 window.Webflow || [];
 window.Webflow.push(() => {
   mouseTrail();
   convertMarkdownToTable();
   testimonialAvatar();
+  reasonGenerator();
   skipToMainContent();
   tallyModal();
   currentYear();
