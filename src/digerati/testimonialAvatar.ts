b8d7d77b7bb05ca246766273ae181fa2f6d1ccb5
Digerati <cabal@digerati.design>
2025-05-23 18:22:01 +0100

    Testing

--- src/digerati/testimonialAvatar.ts
+++ src/digerati/testimonialAvatar.ts
@@ -4,15 +4,15 @@
  * @author <cabal@digerati.design>
  */
 export const testimonialAvatar = () => {
-    const avatars = document.querySelectorAll('.testimonial_photo');
+    const avatars = document.querySelectorAll('.testimonial_half-tone');
     if (!avatars.length) {
         console.log('No avatars found.');
         return;
     }
     avatars.forEach(img => {
         new BreathingHalftone(img, {
-            dotSize: 1 / 80,
-            dotSizeThreshold: 0.1,
+            dotSize: 1 / 120,
+            dotSizeThreshold: 0.025,
             initVelocity: 0.02,
             oscPeriod: 3,
             oscAmplitude: 0.2,
